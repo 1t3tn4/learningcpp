c68938d6b3981a48479f2a42bc35d260e3016dcd
Itetna <119170128+1t3tn4@users.noreply.github.com>
2022-11-27 17:07:49 +0100

    Working With Strings
    5 Lessons Done.

---
+++ strings.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string str1 = "I am the first string! ";
+	string str2 = "And I am the second string!";
+
+	cout << "String One: " << str1 << endl;
+	cout << "String Two: " << str2 << endl;
+	cout << "Both of them combined: " << str1 + str2 << endl;
+
+	
+
+	return 0;
+}
