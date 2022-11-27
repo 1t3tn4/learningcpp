f6e530686e7fe1afde0e19ceb5cd94c482ff5cc0
Itetna <119170128+1t3tn4@users.noreply.github.com>
2022-11-27 15:59:20 +0100

    Getting Input with Cin
    This is a "Plus Calculator" with 3 Integer Numbers

---
+++ cininput.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	// This is a "Plus Calculator" with 3 Integer Numbers
+
+	int a, b, c, d;
+
+	cout << "First Number: ";
+	cin >> a;
+	cout << "Second Number: ";
+	cin >> b;
+	cout << "Third Number: ";
+	cin >> c;
+	d = a + b + c;
+	cout << "Your Result: " << d;
+
+	return 0;
+}
