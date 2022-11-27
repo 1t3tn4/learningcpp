cb5e7c0fc0acdd5435f913ec3b93c4aa8a10bbf1
Itetna <119170128+1t3tn4@users.noreply.github.com>
2022-11-27 15:53:31 +0100

    Update operators.cpp

--- operators.cpp
+++ operators.cpp
@@ -11,6 +11,15 @@ int main() {
 	cout << "Number1 - Number2: " << number1 - number2 << endl;
 	cout << "Number1 * Number2: " << number1 * number2 << endl;
 	cout << "Number1 / Number2: " << float(number1) / number2 << endl;
+	
+	int a = 5;
+	
+	//a *= 2;
+	//a /= 2;
+	//a -= 2;
+	//a += 2;
+	//a++;
+	//a--;
 
 	return 0;
 }
