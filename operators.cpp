da6bd67b908f6392c4c7cde29f546fb5f89a634a
Itetna <119170128+1t3tn4@users.noreply.github.com>
2022-11-27 15:50:49 +0100

    Operators (3)
    3 Lessons Done.

---
+++ operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int number1 = 10;
+	int number2 = 4;
+
+	cout << "Number1 + Number2: " << number1 + number2 << endl;
+	cout << "Number1 - Number2: " << number1 - number2 << endl;
+	cout << "Number1 * Number2: " << number1 * number2 << endl;
+	cout << "Number1 / Number2: " << number1 / number2 << endl;
+
+	return 0;
+}
