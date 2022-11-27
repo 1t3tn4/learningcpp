c9679138d410efd581812c08ed06f3d513f41cc5
Itetna <119170128+1t3tn4@users.noreply.github.com>
2022-11-27 15:47:01 +0100

    Variables (2)
    2 Lessons Done.

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a = 31;
+	float b = 53.35;
+	double c = 34.222;
+	char d = 'A';
+	bool e = false;
+
+	cout << "Variable a has the value of: " << a << endl;
+	cout << "Variable b has the value of: " << b << endl;
+	cout << "Variable c has the value of: " << c << endl;
+	cout << "Variable d has the value of: " << d << endl;
+	cout << "Variable e has the value of: " << e << endl;
+}
